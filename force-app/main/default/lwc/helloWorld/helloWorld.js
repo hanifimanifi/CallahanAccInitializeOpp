5068f789fc8eeda527a0740f90f02595784652b0
hanifimanifi <hanifimanifi@gmail.com>
2023-01-09 22:11:16 -0700

    first commit

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,3 +1,13 @@
 import { LightningElement } from 'lwc';
 
-export default class HelloWorld extends LightningElement {}
+export default class helloWorld extends LightningElement {
+    name //; // undefined 
+    fullname = 'Salesforce LWC'//;
+    age = 25 //;
+    location = {
+        city:"Houston",
+        country : "United States",
+        postalCode : "456000"    
+    };
+    fruits = ["Apple" , "Orange" , "Papaya", " Banana" , "Pineapple"];
+}
